be17613ac5566be6ec2ffd589706088333860332
Noah Padilla <npizza10@gmail.com>
2020-09-04 19:51:34 -0600

    Initial Commit

---
+++ DeleteAllComments.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+To compile go to cmd and type these in respective order.
+	
+	1. gcc <filename>.c -o filename
+	2. ./<filename>
+*/
+
+
+int main(){
+    
+   
+    
+	return 0;
+}
